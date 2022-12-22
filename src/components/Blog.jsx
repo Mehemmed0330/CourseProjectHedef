d496f0d80623826695172329d69c6a57147f4c20
mehemmedeliyevn0330 <mehemmedeliyevn0330@gmail.com>
2022-12-22 15:41:48 +0400

    Update

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -9,7 +9,7 @@ export default function Blog() {
                 </div>
                 <div className='mt-10'>
                     <a href="/" className="mt-2 mx-auto flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-[100%] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <img className="object-cover w-full h-[26rem] rounded-t-lg  md:w-[23rem] md:rounded-none md:rounded-l-lg" src={require("../assets/309696525_686406649483698_1989807744559745647_n.jpg")} alt="" />
+                        <img className="object-cover w-full h-[26rem] rounded-t-lg  md:w-[23rem] md:rounded-none md:rounded-l-lg " src={require("../assets/309696525_686406649483698_1989807744559745647_n.jpg")} alt="" />
                         <div className="flex flex-col justify-between px-4 leading-normal">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of rder.enterprise technology acquisitions of rder.enterprise technology acquisitions of rder.enterprise technology acquisitions of rder.enterprise technology acquisitions of rder.enterprise technology acquisitions of rder.enterprise technology acquisitions of rder.</p>
